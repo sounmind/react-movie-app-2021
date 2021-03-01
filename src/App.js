9ad0487f4e3744d013d2befc9d2c06b4800fb820
sounmind <sounmind777@gmail.com>
2021-03-01 15:55:33 +0900

    practice prop-types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import CustomComponent from "./CustomComponent";
+import { foodILike } from "./data";
+import PropTypes from "prop-types";
+
+function Food(properties) {
+  const { foodName, imageSource, rating } = properties;
+  return (
+    <div>
+      <h1> I like {foodName}</h1>
+      <h2>{rating}/5.0</h2>
+      <img alt={foodName} src={imageSource}></img>
+    </div>
+  );
+}
+
+Food.propTypes = {
+  foodName: PropTypes.string.isRequired,
+  imageSource: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
+
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      {foodILike.map((food) => {
+        return (
+          <Food
+            key={food.id}
+            foodName={food.name}
+            imageSource={food.image}
+            rating={food.rating}
+          ></Food>
+        );
+      })}
+      <CustomComponent></CustomComponent>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/CustomComponent.js
@@ -0,0 +1,5 @@
+const CustomComponent = () => {
+  return <h3>I love you</h3>;
+};
+
+export default CustomComponent;
