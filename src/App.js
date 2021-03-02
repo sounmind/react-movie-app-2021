7a2427e1e2ed1437f56b766b45d231ab195030e0
sounmind <sounmind777@gmail.com>
2021-03-02 15:03:45 +0900

    practice(calculator)
    - if program call setState() and react will also call render() with new state

--- src/App.js
+++ src/App.js
@@ -1,41 +1,25 @@
-import CustomComponent from "./CustomComponent";
-import { foodILike } from "./data";
+import React from "react";
 import PropTypes from "prop-types";
 
-function Food(properties) {
-  const { foodName, imageSource, rating } = properties;
-  return (
-    <div>
-      <h1> I like {foodName}</h1>
-      <h2>{rating}/5.0</h2>
-      <img alt={foodName} src={imageSource}></img>
-    </div>
-  );
-}
-
-Food.propTypes = {
-  foodName: PropTypes.string.isRequired,
-  imageSource: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-      {foodILike.map((food) => {
-        return (
-          <Food
-            key={food.id}
-            foodName={food.name}
-            imageSource={food.image}
-            rating={food.rating}
-          ></Food>
-        );
-      })}
-      <CustomComponent></CustomComponent>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
